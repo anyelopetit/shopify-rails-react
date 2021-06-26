ee597b3a03d4e8e1881315dcdda96907637d8f06
Anyelo <anyelopetit@gmail.com>
2021-06-26 12:02:59 -0400

    getOrders from Rails API

--- frontend-react-bootstrap/src/containers/OrdersContainer.js
+++ frontend-react-bootstrap/src/containers/OrdersContainer.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import getOrders from '../actions/getOrders';
 
 class OrdersContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            orders: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('OrdersContainer was mounted')
+    let orders = this.props.getOrders();
+    console.log('orders: ', orders)
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>OrdersContainer</h1>
+        if (this.props.orders.orders) {
+          this.props.orders
+        } else {
+          <h2>No orders yet.</h2>
         }
-    }
-
-    componentDidMount() {
-        console.log('OrdersContainer was mount')
-    }
-
-    render() {
-        return(<h1>OrdersContainer</h1>)
-    }
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    return { orders: state.orders };
+  return { orders: state.orders };
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        fetchOrders: () => dispatch()
-    }
+  return {
+    getOrders: () => dispatch(getOrders())
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(OrdersContainer);
