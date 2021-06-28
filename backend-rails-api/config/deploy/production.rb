27ecbc52fdab936b3dcc0047d99506ab207f7822
Anyelo <anyelopetit@gmail.com>
2021-06-28 16:35:09 -0400

    change server IP

--- backend-rails-api/config/deploy/production.rb
+++ backend-rails-api/config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '54.76.206.93', user: 'deploy', roles: %w[app db web]
+server '3.248.187.15', user: 'deploy', roles: %w[app db web]
 # server 'example.com', user: 'deploy', roles: %w[app web], other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w[db]
 set :rails_env, 'production'
