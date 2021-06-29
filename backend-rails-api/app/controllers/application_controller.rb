d0644fdf6a2cabefe2ec741aa69af3a9c0764a41
Anyelo <anyelopetit@gmail.com>
2021-06-29 13:03:25 -0400

    test root index

--- backend-rails-api/app/controllers/application_controller.rb
+++ backend-rails-api/app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::API
   include ApiRequestable
+
+  def index
+    render json: { say_hi: 'Hello' }
+  end
 end
--- backend-rails-api/config/routes.rb
+++ backend-rails-api/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # API
+  root to: 'application#index'
   draw :v1
 
   match '*path', to: 'shopify#index', via: :all
